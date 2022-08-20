4856d885c9e4acb15e42034d04ac847c6b2fbfa0
ZavadskyA <forbo1919@gmail.com>
2022-08-20 11:04:57 +0300

    module1: tasks 1, 2 - done

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
   // ваш код...
-}
+  let result = 1;
+  if (n === 0) 
+    return result;
+  else if (n > 0 && (Number.isInteger(n))) {
+    while (n > 0) {
+    result *= n;
+    n--; 
+    } return result;
+  }
+  else return 'Please insert a intenger value or use the Gamma Function!';
+  }
