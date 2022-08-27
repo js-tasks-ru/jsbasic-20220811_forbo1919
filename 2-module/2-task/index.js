933e2954f70383517b121924c369ccc9e33397ba
ZavadskyA <forbo1919@gmail.com>
2022-08-27 11:47:46 +0300

    2-module completed

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
 function isEmpty(obj) {
   // ваш код...
+  //var1
+  // let counter =0;
+
+  // for (let key in obj) counter++;
+  // return !Boolean( counter );
+
+  // var2
+  return Object.keys(obj).length ? false : true;
 }
