656ada05ed2ee3be3ff7598889ec2c3caa6f0bae
ZavadskyA <forbo1919@gmail.com>
2022-08-30 22:51:24 +0300

    3-module completed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
   // ваш код...
+  return users
+  .filter( item =>  item.age <= age )
+  .reduce( (sum, item) => 
+    sum === '' ? `${item.name}, ${item.balance}` 
+    : ( `${sum}\n${item.name}, ${item.balance}` ), '' );
+
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
   // ваш код...
+
+  let numberArr = str
+  .split(' ')
+  .filter( item => isFinite(item) ); 
+  
+  return {
+    min: numberArr.reduce((a, b) => Math.min(a, b)),
+    max: numberArr.reduce((a, b) => Math.max(a, b))
+  }
+  
 }
