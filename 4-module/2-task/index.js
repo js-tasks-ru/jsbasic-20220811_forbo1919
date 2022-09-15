4883e4a8c02f015a848214806c6d2306ba516d4f
ZavadskyA <forbo1919@gmail.com>
2022-09-15 21:41:29 +0300

    Please check errors. It's works with browser but not pass the tests.

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,15 @@
 function makeDiagonalRed(table) {
   // ваш код...
+
+  // let cellsList = document.querySelector('tbody');
+
+  let cellsList = document.querySelector('table');
+
+  for (let i = 0; i < cellsList.rows.length; i++) {
+
+    //cellsList.children[i].children[i].style.backgroundColor = 'red'
+    cellsList.rows[i].cells[i].style.backgroundColor = 'red';
+    //cellsList.rows[i].cells[i].setAttribute('backgroundColor', 'red');
+   
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
   // ваш код...
+
+  let cellsList = document.querySelector('tbody');
+
+  //console.log( cellsList )
+
+  for (let rowLine of cellsList.rows ) {
+    
+    console.log( rowLine.cells[3].dataset.available )
+
+    if (rowLine.cells[3].hasAttribute('data-available') !== true ) {rowLine.hidden = true}
+    else if (rowLine.cells[3].dataset.available = true) {rowLine.classList.add('Available')}
+    else if (rowLine.cells[3].dataset.available = false ) {rowLine.classList.add('Unavailable')}
+
+    console.log(rowLine.cells[2].innerHTML)
+
+    if ( rowLine.cells[2].innerHTML == 'm' ) rowLine.classList.add('male')
+    else if ( rowLine.cells[2].innerHTML == 'f') rowLine.classList.add('female')
+
+    if (rowLine.cells[1].innerHTML < 18) rowLine.style="text-decoration: line-through"
+     
+    console.log(rowLine)
+
+
+
+  }
+
+
+
 }
